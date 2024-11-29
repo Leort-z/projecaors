7704702a6a62f1fef937e37e32a2139c45d95b89
Leonardo Teixeira <leonardorteixeira@hotmail.com>
2024-11-28 22:42:34 -0300

    wip: home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+import './globals.css'
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='pt-br'>
+      <body className='bg-[#F8F1E7] text-[#151318] h-screen'>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
-  return <>hi</>
+  return (
+    <>
+      <header
+        style={{ background: 'linear-gradient(360deg, rgba(15, 30, 21, 0) 0%, #0F1E15 0%)' }}
+        className='w-full flex justify-between items-center pl-10 pr-32 text-xl min-h-40'
+      >
+        <Image src='/white-logo.png' width={200} height={125} alt='Logo da empresa Projeção' />
+        <nav className='flex justify-between gap-16 text-[#F8F1E7] '>
+          <Link href='/portfolio'>PORTFÓLIO</Link>
+          <Link href='/services'>SERVIÇOS</Link>
+          <Link href='/about'>QUEM SOMOS</Link>
+          <Link href='/contact'>CONTATO</Link>
+        </nav>
+      </header>
+      <main className="bg-[url('/main-image.png')] min-h-screen bg-cover bg-no-repeat	"></main>
+    </>
+  )
 }
