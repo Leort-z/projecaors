070186c6637492dd49128b152204e4dd11bd031e
Alisson Silveira <alisson-sds@hotmail.com>
2025-01-03 15:34:52 -0300

    oiiii

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <>
       <header
         style={{ background: 'linear-gradient(360deg, rgba(15, 30, 21, 0) 0%, #0F1E15 0%)' }}
-        className='w-full flex justify-between items-center pl-10 pr-32 text-xl min-h-40'
+        className='w-full flex justify-between items-center pl-40 pr-32 text-x min-h-24'
       >
-        <Image src='/white-logo.png' width={200} height={125} alt='Logo da empresa Projeção' />
+        <Image src='/white-logo.png' width={140} height={87.5} alt='Logo da empresa Projeção' />
         <nav className='flex justify-between gap-16 text-[#F8F1E7] '>
           <Link href='/portfolio'>PORTFÓLIO</Link>
           <Link href='/services'>SERVIÇOS</Link>
