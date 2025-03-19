4965653ebb985ead6c00bd015aa52c175126f29d
Alisson Silveira <alisson-sds@hotmail.com>
2025-03-19 15:59:51 -0300

    Responsividade titulo item do portfolio

--- app/components/PortfolioItem.tsx
+++ app/components/PortfolioItem.tsx
@@ -12,7 +12,7 @@ export default function PortfolioItem({ name, imagePath, id }: PortfolioItemProp
         <div className='h-[68vh] lg:w-1/6 w-full flex flex-col' >
             <div className="h-[60vh] bg-cover min-h-80 bg-center rounded flex items-end justify-center pb-4"
                 style={{ backgroundImage: `url('/portfolio-images/${imagePath}` }}>
-                <Link className="flex items-center justify-center h-12 w-2/4 text-lg bg-amber-50 border-2 border-yellow-600 text-[#A77A37] hover:bg-opacity-80 transition-colors duration-200"
+                <Link className="flex items-center justify-center h-12 w-3/5 text-lg bg-amber-50 border-2 border-yellow-600 text-[#A77A37] hover:bg-opacity-80 transition-colors duration-200"
                     href={`/portfolio/${id}`}> SABER MAIS</Link>
             </div>
             <h3 className="text-2xl">{name}</h3>
