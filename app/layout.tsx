341e80536d1ea46e383d4725b4192513b2bb10e2
Alisson Silveira <alisson-sds@hotmail.com>
2025-04-01 17:42:38 -0300

    Adicionando favicon na pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import { Metadata } from 'next'
+import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: "Projeção Engenharia - Construção e Projetos | Projetos de Engenharia em Capão da Canoa",
   description: "Projeção Engenharia: Desenvolvemos projetos estruturais e laudos técnicos para Capão da Canoa e região. Soluções personalizadas para obras residenciais e comerciais com garantia de qualidade.",
+  icons: {
+    icon: "/favicon.ico"
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
